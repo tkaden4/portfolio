a03a4a13191828ee4da8fce3585b1b412a1d9a8a
Kaden Thomas <thomas.kaden4@outlook.com>
2018-08-17 22:42:58 -0700

    Move to HashRouter to fix SPA issues on github pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./components/App";
 
@@ -13,8 +13,8 @@ if (!root_el) {
 }
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>,
+    </HashRouter>,
     root_el
 );
